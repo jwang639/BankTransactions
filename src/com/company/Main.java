61de3d55bbeb26f3b37e6dca7688d35fa70c8c6b
jwang639 <jwang639@ucr.edu>
2021-07-14 14:36:15 -0700

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("***********************");
+        System.out.println("*************************");
         System.out.println("1, Show Account Balance");
         System.out.println("2, Deposit Money");
         System.out.println("3, Withdraw Money");
-        System.out.println("***********************");
+        System.out.println("*************************");
         System.out.println("Please enter the number above or '0' to exit the program...");
         bankAccount Jiahao= new bankAccount("Jiahao");
         Scanner scanner= new Scanner(System.in);
