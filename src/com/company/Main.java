27e26e0c896f012f33ef5e25f858d4fa0ed6a004
jwang639 <jwang639@ucr.edu>
2021-07-16 00:02:26 -0700

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         bankAccountDao BOA= new bankAccountDaoImpl();
+        BOA.getAccount(0).setCustomerName("Jiahao");
+        BOA.getAccount(0).setPhoneNumber(123);
+        BOA.getAccount(0).deposit(500);
+        BOA.getAccount(1).setCustomerName("Wang");
         for(bankAccount bankAccount: BOA.getAllAccounts()){
             System.out.println("Account Number: "+ bankAccount.getAccountNumber()+ " Account Name: "+
                     bankAccount.getCustomerName());
         }
+        BOA.getAccount(0).getBalance();
+        BOA.getAccount(0).deposit(1);
     }
 }
